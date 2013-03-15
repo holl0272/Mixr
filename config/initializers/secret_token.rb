ae1501fd3d46ccf3ea488f3cf476c34b3bc0a58d
Eric Holland <eric@f-robots.com>
2013-03-15 17:21:38 -0400

    add mixr rails file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mixr::Application.config.secret_token = '7a5890e31c47e6d59c86bdb52331a51f838bc41942540d1b4c643e5be6075f70960a29a36051d58c444b9564a383e9d24466bfd4f1dc39682139ff9d00edb9f9'
